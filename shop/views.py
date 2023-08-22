4d653d5c4091a4fee03d4d19028a5ad6bedd6576
ritikchaurasia <ritikchaurasia868@gmail.com>
2023-08-22 17:39:48 +0530

    update

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product, Contact,Orders
+from .models import Product, Contact,Orders,OrderUpdate
 from math import ceil
+import json
 
 
 # Create your views here.
 def index(request):
-    # products = Product.objects.all()
-    # print(products)
-    # n = len(products)
-    # nSlides = n//4 + ceil((n/4)-(n//4))
     allProds =[]
     catprods = Product.objects.values('category','id')
     cats = {item['category'] for item in catprods}
@@ -18,10 +15,6 @@ def index(request):
         n = len(prod)
         nSlides = n//4 + ceil((n/4)-(n//4))
         allProds.append([prod,range(1,nSlides),nSlides])
-
-    #params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
-    # allProds = [[products,range(1,nSlides),nSlides],
-    #             [products,range(1,nSlides),nSlides]]
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
@@ -41,6 +34,22 @@ def contact(request):
     return render(request, 'shop/contact.html')
 
 def tracker(request):
+    if request.method =="POST":
+        orderId =  request.POST.get('orderId','')
+        email = request.POST.get('email','')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text':item.update_desc,'time':item.timestamp})
+                    response = json.dumps(updates,default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
     return render(request, 'shop/tracker.html')
 
 def search(request):
@@ -49,7 +58,6 @@ def search(request):
 def productView(request,myid):
     # Fetch the product using the id 
     product = Product.objects.filter(id=myid)
-    #print(product)
     return render(request, 'shop/prodView.html',{'product':product[0]})
 
 def checkout(request):
@@ -66,6 +74,8 @@ def checkout(request):
         order = Orders(items_json=items_json,name=name,email=email,address=address,
                        city=city,state=state,zip_code=zip_code,phone=phone)
         order.save()
+        update= OrderUpdate(order_id=order.order_id,update_desc="The order has been placed")
+        update.save()
         thank = True
         id = order.order_id
         return render(request, 'shop/checkout.html',{'thank':thank,'id':id})
