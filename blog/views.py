225f7fdd3b8e72833f4f30d44f3bbcd1345392b6
ritikchaurasia <ritikchaurasia868@gmail.com>
2023-08-25 17:47:17 +0530

    update

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,9 @@ from .models import Blogpost
 # Create your views here.
 
 def index(request):
-    return render(request,"blog/index.html")
+    myposts = Blogpost.objects.all()
+    print(myposts)
+    return render(request,"blog/index.html",{'myposts':myposts})
 
 def blogpost(request,id):
     post = Blogpost.objects.filter(post_id=id)[0]
