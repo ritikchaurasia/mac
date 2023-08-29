0e06f755d6fb019a379b23dc04e0d9eaaa6cf2cd
ritikchaurasia <ritikchaurasia868@gmail.com>
2023-08-29 18:14:45 +0530

    update

--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,6 @@ from .models import Product, Contact,Orders,OrderUpdate
 from math import ceil
 import json
 
-
 # Create your views here.
 def index(request):
     allProds =[]
@@ -17,6 +16,31 @@ def index(request):
         allProds.append([prod,range(1,nSlides),nSlides])
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
+
+def searchMatch(query,item):
+    # return  true only if query  matches  the item 
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query = request.GET.get('search')
+    allProds =[]
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp =  Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query,item)]
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4)-(n//4))
+        if len(prod) !=0:
+            allProds.append([prod,range(1,nSlides),nSlides])
+    params = {'allProds':allProds,"msg":""}
+    if len(allProds) == 0 or len(query)<4:
+        params = {'msg':"Please make sure to enter relevant search query "}
+    return render(request, 'shop/search.html', params)
+
 
 def about(request):
     return render(request, 'shop/about.html')
@@ -54,8 +78,7 @@ def tracker(request):
             return HttpResponse('{}')
     return render(request, 'shop/tracker.html')
 
-def search(request):
-    return render(request, 'shop/search.html')
+
 
 def productView(request,myid):
     # Fetch the product using the id 
@@ -66,6 +89,7 @@ def checkout(request):
     if request.method =="POST":
         items_json = request.POST.get('itemsJson','')
         name =  request.POST.get('name','')
+        amount = request.POST.get('amount','')
         email = request.POST.get('email','')
         address =  request.POST.get('address1','') + " " + request.POST.get('address2','')
         city = request.POST.get('city','')
@@ -74,7 +98,7 @@ def checkout(request):
         phone =  request.POST.get('phone','')
 
         order = Orders(items_json=items_json,name=name,email=email,address=address,
-                       city=city,state=state,zip_code=zip_code,phone=phone)
+                       city=city,state=state,zip_code=zip_code,phone=phone,amount=amount)
         order.save()
         update= OrderUpdate(order_id=order.order_id,update_desc="The order has been placed")
         update.save()
@@ -82,3 +106,4 @@ def checkout(request):
         id = order.order_id
         return render(request, 'shop/checkout.html',{'thank':thank,'id':id})
     return render(request, 'shop/checkout.html')
+
